cd59295c8fb06eec8364634d8e1e363660ac6219
Jonathan <jmedel.7@gmail.com>
2020-02-27 17:53:01 -0300

    client pets and pethistory

---
+++ app/models/pet.rb
@@ -0,0 +1,32 @@
+class Pet < ApplicationRecord
+    has_many :pethistories
+    belongs_to :client
+
+    def pet_visits
+        pethistories.count
+    end
+
+    def pet_weight
+        pethistories.pluck(:weight)[0] #el [0] es para sacar el primer valor del array 
+    end
+
+    def pet_height
+        pethistories.pluck(:height)[0]
+    end
+
+    def avg_weight
+        pethistories.average(:weight)
+    end
+
+    def avg_height
+        pethistories.average(:height)
+    end
+
+    def max_weight
+        pethistories.pluck(:weight).max
+    end
+
+    def max_height
+        pethistories.pluck(:height).max
+    end
+end
